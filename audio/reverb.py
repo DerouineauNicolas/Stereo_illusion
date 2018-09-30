6c68272a71e2f9c78099691ef1d30950280149af
Nicolas DEROUINEAU <nicodero72@gmail.com>
2018-09-30 17:22:29 +0200

    Code cleaning

--- audio/reverb.py
+++ audio/reverb.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python3
 #Simple delay line Reverb
- 
+
 
 import numpy as np
 import sounddevice as sd
 import wave as wavereader
 
-sd.default.samplerate = 44100
-sd.default.channels = 2
-
-time = 10.0
-frequency1 = 440
-frequency2 = 460
-
 waveinst = wavereader.open("Guitar-C3.wav")
 wavebytes = waveinst.readframes(waveinst.getnframes());
 
 wavetmp = np.frombuffer(wavebytes, dtype="int16")
-wave=np.copy(wavetmp)
-
-print ("wave.size =", wave.size)
-
+wave = np.copy(wavetmp)
 
 decay = 0.8
 delayMilliseconds = 500
@@ -31,7 +21,8 @@ print (delaySamples)
 for i in range(1,(len(wave)-delaySamples)):
     wave[i + delaySamples] += wave[i] * decay
 
-
+sd.default.samplerate = waveinst.getframerate()
+sd.default.channels = 2
 
 print ("Playing sound with no reverb")
 
@@ -45,3 +36,5 @@ result = np.hstack((wave.reshape(-1, 1), wave.reshape(-1,1)))
 wav_wave = np.array(result, dtype=np.int16)
 
 sd.play(wav_wave, blocking=True)
+
+waveinst.close()
